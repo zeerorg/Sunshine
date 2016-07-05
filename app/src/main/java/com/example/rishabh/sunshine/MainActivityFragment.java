d94810a76630096147e4c22fe7fcbd7d4065c86d
zeerorg <r.g.gupta@outlook.com>
2016-07-05 15:09:30 +0530

    Weather data updates and is shown on device

--- app/src/main/java/com/example/rishabh/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/rishabh/sunshine/MainActivityFragment.java
@@ -14,18 +14,27 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * A placeholder fragment containing a simple view.
  */
+
 public class MainActivityFragment extends Fragment {
+
+    public static ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -54,7 +63,7 @@ public class MainActivityFragment extends Fragment {
         ArrayList<String> dataArray = new ArrayList<String>();
         for (String obj : data)
             dataArray.add(obj);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        adapter = new ArrayAdapter<String>(
                 getActivity(),
                 R.layout.list_item_forecast,
                 R.id.list_item_forecast_textview,
@@ -82,11 +91,11 @@ public class MainActivityFragment extends Fragment {
     }
 }
 
-class FetchWeather extends AsyncTask<String, Void, Void> {
+class FetchWeather extends AsyncTask<String, Void, String[]> {
 
     private final String LOG_TAG = FetchWeather.class.getSimpleName();
 
-    protected Void doInBackground(String... params) {
+    protected String[] doInBackground(String... params) {
 
         // These two need to be declared outside the try/catch
         // so that they can be closed in the finally block.
@@ -112,7 +121,7 @@ class FetchWeather extends AsyncTask<String, Void, Void> {
                     .appendQueryParameter("units", "metric")
                     .appendQueryParameter("cnt", "7")
                     .appendQueryParameter("appid", "5280b4b78495d50e61ab8bee22c7a59a");
-            Log.e(LOG_TAG, buildURL.build().toString());
+            Log.v(LOG_TAG, buildURL.build().toString());
             URL url = new URL(buildURL.build().toString());
 
 
@@ -156,11 +165,99 @@ class FetchWeather extends AsyncTask<String, Void, Void> {
                 try {
                     reader.close();
                 } catch (final IOException e) {
-                    Log.e(LOG_TAG, "Error closing stream", e);
                 }
             }
         }
-        Log.e(LOG_TAG, forecastJsonStr);
-        return null;
+        try {
+            String[] results = getWeatherDataFromJson(forecastJsonStr, 7);
+            return results;
+        }
+        catch(JSONException e){
+            return null;
+        }
+    }
+
+    private String getReadableDateString(long time){
+        // Because the API returns a unix timestamp (measured in seconds),
+        // it must be converted to milliseconds in order to be converted to valid date.
+        Date date = new Date(time * 1000);
+        SimpleDateFormat format = new SimpleDateFormat("E, MMM d");
+        return format.format(date).toString();
+    }
+
+    /**
+     * Prepare the weather high/lows for presentation.
+     */
+    private String formatHighLows(double high, double low) {
+        // For presentation, assume the user doesn't care about tenths of a degree.
+        long roundedHigh = Math.round(high);
+        long roundedLow = Math.round(low);
+
+        String highLowStr = roundedHigh + "/" + roundedLow;
+        return highLowStr;
+    }
+
+    /**
+     * Take the String representing the complete forecast in JSON Format and
+     * pull out the data we need to construct the Strings needed for the wireframes.
+     *
+     * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+     * into an Object hierarchy for us.
+     */
+    private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+            throws JSONException {
+
+        // These are the names of the JSON objects that need to be extracted.
+        final String OWM_LIST = "list";
+        final String OWM_WEATHER = "weather";
+        final String OWM_TEMPERATURE = "temp";
+        final String OWM_MAX = "max";
+        final String OWM_MIN = "min";
+        final String OWM_DATETIME = "dt";
+        final String OWM_DESCRIPTION = "main";
+
+        JSONObject forecastJson = new JSONObject(forecastJsonStr);
+        JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+        String[] resultStrs = new String[numDays];
+        for(int i = 0; i < weatherArray.length(); i++) {
+            // For now, using the format "Day, description, hi/low"
+            String day;
+            String description;
+            String highAndLow;
+
+            // Get the JSON object representing the day
+            JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+            // The date/time is returned as a long.  We need to convert that
+            // into something human-readable, since most people won't read "1400356800" as
+            // "this saturday".
+            long dateTime = dayForecast.getLong(OWM_DATETIME);
+            day = getReadableDateString(dateTime);
+
+            // description is in a child array called "weather", which is 1 element long.
+            JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+            description = weatherObject.getString(OWM_DESCRIPTION);
+
+            // Temperatures are in a child object called "temp".  Try not to name variables
+            // "temp" when working with temperature.  It confuses everybody.
+            JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+            double high = temperatureObject.getDouble(OWM_MAX);
+            double low = temperatureObject.getDouble(OWM_MIN);
+
+            highAndLow = formatHighLows(high, low);
+            resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            //System.out.println(resultStrs[0]);
+        }
+
+        return resultStrs;
+    }
+
+    public void onPostExecute(String[] data){
+        MainActivityFragment.adapter.clear();
+        for(String s: data){
+            MainActivityFragment.adapter.add(s);
+        }
+
     }
 }
